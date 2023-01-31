5b484bd3f248af7b48a05672ac96b5e74c8c17cc
Bruno Arouca ITS <brunoarouca.dev@gmail.com>
2023-01-31 15:14:39 -0300

    Parte lógica com javascript finalizada

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,34 @@
 const multiplicationForm = document.querySelector('#multiplication-form');
 const numberInput = document.querySelector('#number');
 const multiplicationInput = document.querySelector('#multiplicator');
-
-
+const multiplicationTable = document.querySelector('#multiplication-operations')
 
 // Funções
+const createTable = (number, multiplicatorNumber) => {
 
+    multiplicationTable.innerHTML = ""; 
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+
+        const result = number * i;
+
+        const template = 
+        `   <div class="row">
+                <div class="operation">${number} x ${i} = </div>
+                <div class="result">${result}</div> 
+            </div>  
+        `
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector('.row');
+
+        multiplicationTable.appendChild(row)
+    }
+
+    
+
+}
 
 // Eventos
 
@@ -16,5 +39,8 @@ multiplicationForm.addEventListener('submit', (e) => {
     const multiplicationNumber = +numberInput.value;
     const multiplicatorNumber = +multiplicationInput.value;
 
-    console.log(multiplicationNumber, multiplicatorNumber)
+    if (!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber)
+
 })
